88ddd2759e662f5c0a40620944e15ab4d3e591b2
comp3004Final <152740515+comp3004Final@users.noreply.github.com>
2023-12-04 19:38:39 -0500

    Add files via upload
    Added 2nd step

--- AEDproject/mainwindow.cpp
+++ AEDproject/mainwindow.cpp
@@ -5,6 +5,9 @@
 #include <QPixmap>
 #include <QLabel>
 
+// Member variable for the flashing lights
+QTimer *flashTimer;
+
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) , ui(new Ui::MainWindow) {
 
     ui->setupUi(this);
@@ -13,6 +16,12 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) , ui(new Ui::MainW
     // 1 = Pass, 0 = Fail
     statusIndicator = 1;
     step = 0;
+
+    //Used for the flashing lights
+    flashTimer = new QTimer(this);
+    connect(flashTimer, &QTimer::timeout, this, [this]() { toggleFlash(ui->step1); });
+    connect(flashTimer, &QTimer::timeout, this, [this]() { toggleFlash(ui->step2); });
+
 
     //Initialize image on power button
     initializePowerButtonImage(ui->powerButton, ":/res/Buttons/powerButton.svg", 50, 50);
@@ -30,9 +39,9 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) , ui(new Ui::MainW
     ui->shockInd->setStyleSheet("QLabel { border: 2px solid black; }");
     ui->statInd->setStyleSheet("QLabel { border: 2px solid black; }");
 
-    setStepBackgroundColor(ui->step1, Qt::yellow);
 
-    setStepBackgroundColor(ui->shockInd, Qt::red);
+
+//    setStepBackgroundColor(ui->shockInd, Qt::red);
 
 
     //------- Electrodes ---------
@@ -50,6 +59,7 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) , ui(new Ui::MainW
 
     // Connect the button's clicked signal to the powerButtonClicked slot
     connect(ui->powerButton, &QPushButton::clicked, this, &MainWindow::powerButtonClicked);
+
 }
 
 void MainWindow::setStepBackgroundColor(QLabel *label, const QColor &color) {
@@ -77,16 +87,19 @@ void MainWindow::runSelfTest() {
 }
 
 void MainWindow::selfTestComplete() {
-    if(statusIndicator == 1){
+    if (statusIndicator == 1) {
         // Display a message in the QTextBrowser after the 5-second delay
-//        ui->LCDScreen->append("Automated Defibrillator OK");
+        ui->LCDScreen->append("Automated Defibrillator OK");
         // Update the statInd QLabel with the pass.svg image
         QPixmap statIndImage(":/res/Buttons/pass.svg");
         ui->statInd->setPixmap(statIndImage.scaled(80, 80, Qt::KeepAspectRatio));
         ui->voiceOutput->setText("Automated Defibrillator OK");
-    } else{
+
+        // Move to the next step after the self-test is complete
+        nextStep();
+    } else {
         // Display a message in the QTextBrowser after the 5-second delay
-//        ui->LCDScreen->append("Automated Defibrillator FAILED");
+        ui->LCDScreen->append("Automated Defibrillator FAILED");
         // Update the statInd QLabel with the pass.svg image
         QPixmap statIndImage(":/res/Buttons/fail.svg");
         ui->statInd->setPixmap(statIndImage.scaled(80, 80, Qt::KeepAspectRatio));
@@ -94,13 +107,11 @@ void MainWindow::selfTestComplete() {
     }
 }
 
-// Modify the powerButtonClicked function to call runSelfTest when powering on
 void MainWindow::powerButtonClicked() {
     if (powerStatus == 0) {
         ui->powerButton->setStyleSheet("background-color: green");
         ui->LCDScreen->setText("AED is powering on...");
         powerStatus = 1;
-
         // Add a 3-second delay before calling the runSelfTest function
         QTimer::singleShot(3000, this, &MainWindow::runSelfTest);
     } else {
@@ -108,7 +119,111 @@ void MainWindow::powerButtonClicked() {
     }
 }
 
+void MainWindow::startFlash(QLabel *label) {
+    // Start the flashing timer for the given label
+    flashTimer->disconnect();  // Disconnect any previous connections
+    connect(flashTimer, &QTimer::timeout, this, [this, label]() { toggleFlash(label); });
+    flashTimer->start(800);  // Adjust the interval (e.g., 800 ms) as needed
+}
+
+void MainWindow::stopFlash(QLabel *label) {
+    // Stop the flashing timer and reset the label to its original color
+    flashTimer->disconnect();
+    flashTimer->stop();
+
+    QPalette palette = label->palette();
+    palette.setColor(QPalette::Window, Qt::white);
+    label->setAutoFillBackground(true);
+    label->setPalette(palette);
+}
+
+void MainWindow::toggleFlash(QLabel *label) {
+    // Toggle the background color between yellow and the original color
+    QPalette palette = label->palette();
+    if (palette.color(QPalette::Window) == Qt::yellow) {
+        palette.setColor(QPalette::Window, Qt::white);
+    } else {
+        palette.setColor(QPalette::Window, Qt::yellow);
+    }
+    label->setAutoFillBackground(true);
+    label->setPalette(palette);
+}
+
+
+void MainWindow::performAEDStep() {
+    switch (step) {
+    case 1:
+            // Step 1: Display "Stay calm." on voiceOutput for 10 seconds
+            ui->voiceOutput->append("Stay calm");
+            ui->voiceOutput->append("Check responsiveness");
+            toggleFlash(ui->step1);  // Toggle the flash for step 1
+
+            // Start the flashing timer for 10 seconds
+            flashTimer->start(10000);
+
+            // Move to the next step after the current step's duration (e.g., 10 seconds)
+            QTimer::singleShot(10000, this, &MainWindow::nextStep);
+            break;
+
+        case 2:
+            // Step 2: Call for help for 10 seconds
+            ui->voiceOutput->append("Call for help");
+            stopFlash(ui->step1);  // Stop the flash for step 1
+            startFlash(ui->step2);  // Start the flash for step 2
+            break;
+
+        // ... (update for other steps)
+
+        default:
+
+            break;
+    }
+}
+
+QLabel *MainWindow::getStepLabel(int step) {
+    switch (step) {
+        case 1:
+            return ui->step1;
+        case 2:
+            return ui->step2;
+        case 3:
+            return ui->step3;
+        case 4:
+            return ui->step4;
+        case 5:
+            return ui->step5;
+        default:
+            return nullptr;
+    }
+}
+
+void MainWindow::nextStep() {
+    // Increment the step variable
+    step++;
+
+    // Perform actions based on the new step
+    performAEDStep();
+}
+
+void MainWindow::stopCurrentStep() {
+    // Stop the flashing and move to the next step
+    stopFlash(ui->step1);
+    stopFlash(ui->step2);
+    // ... (stop other steps)
+
+    // Move to the next step after the current step's duration ( 10 seconds)
+    QTimer::singleShot(10000, this, &MainWindow::nextStep);
+}
+
+void MainWindow::previousStep() {
+    // Decrement the step variable
+    step--;
+
+    // Perform actions based on the new step
+    performAEDStep();
+}
+
 MainWindow::~MainWindow() {
     delete ui;
-//    delete electrodes; // Don't forget to clean up resources
+    delete flashTimer;
 }
--- AEDproject/mainwindow.h
+++ AEDproject/mainwindow.h
@@ -1,8 +1,8 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <electrodes.h>
-#include <voiceprompt.h>
+#include "electrodes.h"
+#include "voiceprompt.h"
 
 #include <QMainWindow>
 #include <QLabel>
@@ -23,15 +23,29 @@ private:
     int statusIndicator;
     int step;
     int powerStatus;
+    QTimer *flashTimer;
     Ui::MainWindow *ui;
     Electrodes *electrodesWidget;
     VoicePrompt *voicePromptWidget;
 
+    void toggleFlash(QLabel *label);
+    void startFlash(QLabel *label);
+    void stopFlash(QLabel *label);
+    void stopCurrentStep();
+    QLabel* getStepLabel(int step);
+
     void runSelfTest();
     void selfTestComplete();
+
     void setStepBackgroundColor(QLabel *label, const QColor &color);
+
     void initializeLabelImage(QLabel *label, const QString &imagePath, int width, int height);
     void initializePowerButtonImage(QPushButton *button, const QString &imagePath, int iconWidth, int iconHeight);
+
     void powerButtonClicked();
+
+    void performAEDStep();
+    void nextStep();
+    void previousStep();
 };
 #endif
